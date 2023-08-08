08fbec919e6d20804583d26b20b85ec2e82be066
OnyeEbube <okontaonyeka@gmail.com>
2023-08-08 19:57:10 +0000

    Adding solutions for task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,10 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/',methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """GET /api/v1/unauthorized
     Return:
       - raises error abort
     """
-    abort(401)
+    abort(401, description='Unauthorized')
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    Return:
+      - raises error abort
+    """
+    abort(403, description='Forbidden')
