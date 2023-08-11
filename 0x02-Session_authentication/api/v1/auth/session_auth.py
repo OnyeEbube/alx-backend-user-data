bb3c53a5706589724a840021cf81a9a5b7df9123
OnyeEbube <okontaonyeka@gmail.com>
2023-08-11 19:50:18 +0000

    Adding solution to task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,9 +5,23 @@ A class
 
 
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """
     Inherits from Auth
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """_summary_
+        """
+        if user_id is None:
+            return None
+        if user_id not isinstance(str):
+            return None
+
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
